c128cb55c48991bbfe1e92e98d9dced6206c70a3
zhanghaitao <stockwyz@163.com>
2017-11-21 19:07:13 +0800

    eureka-server-slave

--- spring-eureka-provider/src/main/java/com/spring/test/eureka/provider/controller/HelloController.java
+++ spring-eureka-provider/src/main/java/com/spring/test/eureka/provider/controller/HelloController.java
@@ -1,13 +1,23 @@
 package com.spring.test.eureka.provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+
+
 @RestController
 public class HelloController {
+	@Autowired
+	private DiscoveryClient client;
 	@RequestMapping(value="/hello", method=RequestMethod.GET)
 	public void hello(String name) {
-		System.out.println("hello," + name);
+		ServiceInstance si = client.getLocalServiceInstance();
+		System.out.println("hello," + si.getHost() + ":" + si.getPort() + ":" + si.getServiceId());
 	}
 }
--- spring-eureka-slave/src/main/java/com/spring/test/eureka/EurekaServer.java
+++ spring-eureka-slave/src/main/java/com/spring/test/eureka/EurekaServer.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class Application {
+public class EurekaServer {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		new SpringApplicationBuilder(EurekaServer.class).web(true).run(args);
 	}
 }
